7b7792a3c45bcd39074c2ebfb9b14be08b3c90d8
Ханецкий Евгений Викторович(103a) <KhanetskiEV@minsk.belpost.by>
2023-04-13 17:15:32 +0300

    init: start what-season

--- src/what-season.js
+++ src/what-season.js
@@ -14,8 +14,10 @@ const { NotImplementedError } = require('../extensions/index.js');
 function getSeason(date) {
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-
-  
+  if (date.getMonth == 2 || date.getMonth == 3 || date.getMonth == 4) return 'spring'
+  if (date.getMonth == 5 || date.getMonth == 6 || date.getMonth == 7) return 'summer'
+  if (date.getMonth == 8 || date.getMonth == 9 || date.getMonth == 10) return 'autumn'
+  if (date.getMonth == 11 || date.getMonth == 0 || date.getMonth == 1) return 'winter'
 }
 
 module.exports = {
